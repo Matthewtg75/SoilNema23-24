4f4e8063bf03604142d2a67782694ef52c693cf8
Matthewtg75 <mtg94503@uga.edu>
2025-10-03 15:43:01 -0400

    Update 2023_continous_ndwi.R

--- 23 and 24 code/2023_continous_ndwi.R
+++ 23 and 24 code/2023_continous_ndwi.R
@@ -25,29 +25,32 @@ cor.test(data$NDWI.avg, data$spiral, method = "pearson")
 model_simple_root <- lm(data$rootknot ~ data$NDWI.avg)
 
 summary(model_simple_root)
-Residuals:
-  Min      1Q  Median      3Q     Max 
--135.21  -82.58  -17.09   98.67  168.84 
+#Residuals:
+#  Min      1Q  Median      3Q     Max 
+#-135.21  -82.58  -17.09   98.67  168.84 
 
-Coefficients:
-              Estimate Std. Error t value Pr(>|t|)  
-(Intercept)     -149.5      143.9  -1.039    0.313  
-data$NDWI.avg    583.5      324.6   1.798    0.089
-Residual standard error: 104.8 on 18 degrees of freedom
-F-statistic: 3.232 on 1 and 18 DF,  p-value: 0.08901
+#Coefficients:
+#              Estimate Std. Error t value Pr(>|t|)  
+#(Intercept)     -149.5      143.9  -1.039    0.313  
+#data$NDWI.avg    583.5      324.6   1.798    0.089
+#Residual standard error: 104.8 on 18 degrees of freedom
+#F-statistic: 3.232 on 1 and 18 DF,  p-value: 0.08901
 
+plot(model_simple_root)
 
 # Simple linear regression
 model_simple_spiral <- lm(data$spiral ~ data$NDWI.avg)
 
 summary(model_simple_spiral)
 #Residuals:
-    Min      1Q  Median      3Q     Max 
--168.93 -104.89  -67.79   -0.42  516.61 
+#    Min      1Q  Median      3Q     Max 
+#-168.93 -104.89  -67.79   -0.42  516.61 
 
 #Coefficients:
-              Estimate Std. Error t value Pr(>|t|)
-(Intercept)     -189.2      256.7  -0.737    0.471
-data$NDWI.avg    642.7      578.9   1.110    0.282
-Residual standard error: 187 on 18 degrees of freedom
-F-statistic: 1.232 on 1 and 18 DF,  p-value: 0.2816
+#              Estimate Std. Error t value Pr(>|t|)
+#(Intercept)     -189.2      256.7  -0.737    0.471
+#data$NDWI.avg    642.7      578.9   1.110    0.282
+#Residual standard error: 187 on 18 degrees of freedom
+#F-statistic: 1.232 on 1 and 18 DF,  p-value: 0.2816
+
+plot(model_simple_spiral)
